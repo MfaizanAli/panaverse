26e6523c9d075fd7ae9912e9bf08851f1fbecc1f
unknown <faizanmalik135@gmail.com>
2022-10-03 14:54:48 +0500

    First commit

---
+++ question10_file1.js
@@ -0,0 +1,9 @@
+/**
+ * author name:     Muhammad Faizan ali
+ * date:            2022-10-03
+ * description      this program will print counting from 0 to 9 on screen 10 times
+ * */
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
---
+++ question10_file2.js
@@ -0,0 +1,7 @@
+/**
+ * author name:     Muhammad Faizan ali
+ * date:            2022-10-03
+ * description      this program will print hello word on screen
+ * */
+
+console.log("Hello Word");
